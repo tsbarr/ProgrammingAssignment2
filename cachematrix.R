f7596be0e352e59396a2caac3a993c6f5fc888ea
tsbarr <63961884+tsbarr@users.noreply.github.com>
2020-05-05 16:33:39 -0400

    Added short comment about makeCacheMatrix()

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
 # These functions calculate the inverse of a matrix using the function solve(),
 # storing the result in cache, so the calculation doesn't have to be repeated.
 
-## Write a short comment describing this function
+## (1) Function that returns a list of 4 functions to set and get from cache
+## the matrix of interest and its inverse. The arg 'x' is the matrix of interest
+## that can be set directly when this function is called, but it can also be
+## set or modified calling the function 'set_matrix' from the list returned. 
+## The other three functions in the list will be used by the next function to:
+## get_matrix = get the matrix from cache,
+## set_inverse = set the inverse to cache after calculating it,
+## get_inverse = get the inverse from cache if previously calculated.
 
 makeCacheMatrix <- function(x = matrix()) {
 
